7cadf4f970626e7ebee608a3420fc82939f409ec
Raul Orban <raulorban1@gmail.com>
2022-05-22 18:37:07 -0400

    Calculate multiple 1 digit numbers in a row

--- calc.js
+++ calc.js
@@ -1,25 +1,68 @@
 const buttons = document.querySelectorAll("#btn")
+const display = document.getElementById("display")
+const clearButton = document.getElementById('clear-button')
+
+let firstNumber = ''
+let secondNumber = ''
+let operator = ''
+
+let equal = '='
 
 for (btn of buttons) {
     btn.addEventListener("click", function(e) {
-    const currentValue = e.target.value 
-    console.log(currentValue);})
+
+    // if (firstNumber && operator) {
+    //     const calcValue = operate(operator, parseInt(firstNumber), parseInt(secondNumber))
+
+    //     console.log(calcValue);
+    //     firstNumber = calcValue
+    //     console.log(firstNumber);
+    // }
+
+    if (e.target.value === "+" || e.target.value === "-" || e.target.value === '*' || e.target.value === '/' ){
+        operator = e.target.value
+    } else if (firstNumber === "") {
+        firstNumber += e.target.value;
+        console.log(firstNumber);
+    } else if (operator){
+        secondNumber += parseInt(e.target.value);
+        console.log(secondNumber);
     }
 
+    if (e.target.value === equal) {
+        const calcValue = operate(operator, parseInt(firstNumber), parseInt(secondNumber))
+        firstNumber = calcValue
+        display.innerHTML = calcValue
+        secondNumber = ''
+        console.log(calcValue);
+    }
+
+    if (e.target.value === "clear") {
+       clearDisplay()
+    } else {
+        //display.innerHTML = 0
+    }
+    }
+)}
+
 function add(a, b) {
-    return a + b
+    result = a + b
+    return result
 }
 
 function subtract(a, b) {
-    return a - b
+    result = a - b
+    return result
 }
 
 function multiply(a, b) {
-    return a * b
+    result = a * b
+    return result
 }
 
 function divide(a, b) {
-    return a / b
+    result = a / b
+    return result
 }
 
 function operate(operator, a, b) {
@@ -34,4 +77,9 @@ function operate(operator, a, b) {
     }
 }
 
-console.log(operate("*", 5, 2));
+function clearDisplay() {
+    firstNumber = ''
+    secondNumber = ''
+    operator = ''
+    display.innerHTML = 0
+}
